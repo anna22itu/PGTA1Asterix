20dae9059e8955ea840857a3df4b96c6f679a063
Àlex González Gámez <alex.gonzalezgamez@gmail.com>
2022-10-01 11:35:09 +0200

    Prueba de cambio

---
+++ PGTA1/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class Main
+    {
+        public static void hola()
+        {
+
+
+        }
+    }
+}
