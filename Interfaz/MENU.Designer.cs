771b6a3acec2a4c70fc657cdf8f112aaed7e5f9d
anna22itu <anna.iturralde.aguilo@gmail.com>
2022-11-20 08:48:17 +0100

    avance simulator
    avance simulator

--- Interfaz/MENU.Designer.cs
+++ Interfaz/MENU.Designer.cs
@@ -31,6 +31,9 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.panelUp = new System.Windows.Forms.Panel();
+            this.panelMENU = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -39,33 +42,64 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(79, 315);
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(400, 512);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 42);
+            this.button1.Size = new System.Drawing.Size(221, 70);
             this.button1.TabIndex = 3;
             this.button1.Text = "OPEN FILE";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(531, 315);
+            this.button2.Location = new System.Drawing.Point(759, 525);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(155, 42);
+            this.button2.Size = new System.Drawing.Size(221, 70);
             this.button2.TabIndex = 4;
             this.button2.Text = "EXPORT";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form1
+            // panelUp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.panelUp.Location = new System.Drawing.Point(533, 132);
+            this.panelUp.Name = "panelUp";
+            this.panelUp.Size = new System.Drawing.Size(300, 150);
+            this.panelUp.TabIndex = 5;
+            // 
+            // panelMENU
+            // 
+            this.panelMENU.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panelMENU.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMENU.Location = new System.Drawing.Point(0, 0);
+            this.panelMENU.Name = "panelMENU";
+            this.panelMENU.Size = new System.Drawing.Size(267, 750);
+            this.panelMENU.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(476, 316);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(300, 150);
+            this.panel3.TabIndex = 7;
+            // 
+            // MENU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1143, 750);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panelMENU);
+            this.Controls.Add(this.panelUp);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MENU";
+            this.Text = "MENU";
             this.ResumeLayout(false);
 
         }
@@ -75,5 +109,8 @@
         private OpenFileDialog openFileDialog1;
         private Button button1;
         private Button button2;
+        private Panel panelUp;
+        private Panel panelMENU;
+        private Panel panel3;
     }
 }
