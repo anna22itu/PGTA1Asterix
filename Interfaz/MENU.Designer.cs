85b79ddd4cdd78f8c704a8e1b12b48969c8ed796
anna22itu <anna.iturralde.aguilo@gmail.com>
2022-11-20 09:15:53 +0100

    avance simulator
    avance simulator

--- Interfaz/MENU.Designer.cs
+++ Interfaz/MENU.Designer.cs
@@ -29,77 +29,257 @@
         private void InitializeComponent()
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.panelUp = new System.Windows.Forms.Panel();
+            this.BtnLoadFile = new System.Windows.Forms.Button();
+            this.BtnExportFile = new System.Windows.Forms.Button();
             this.panelMENU = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnMapView = new System.Windows.Forms.Button();
+            this.BtnAboutUs = new System.Windows.Forms.Button();
+            this.BtnDataView = new System.Windows.Forms.Button();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox3 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox4 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox5 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox6 = new FontAwesome.Sharp.IconPictureBox();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.panelMENU.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // button1
+            // BtnLoadFile
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.button1.Location = new System.Drawing.Point(400, 512);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(221, 70);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "OPEN FILE";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.BtnLoadFile.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.BtnLoadFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLoadFile.Location = new System.Drawing.Point(80, 140);
+            this.BtnLoadFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BtnLoadFile.Name = "BtnLoadFile";
+            this.BtnLoadFile.Size = new System.Drawing.Size(120, 45);
+            this.BtnLoadFile.TabIndex = 3;
+            this.BtnLoadFile.Text = "OPEN FILE";
+            this.BtnLoadFile.UseVisualStyleBackColor = false;
+            this.BtnLoadFile.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // BtnExportFile
             // 
-            this.button2.Location = new System.Drawing.Point(759, 525);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(221, 70);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "EXPORT";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // panelUp
-            // 
-            this.panelUp.Location = new System.Drawing.Point(533, 132);
-            this.panelUp.Name = "panelUp";
-            this.panelUp.Size = new System.Drawing.Size(300, 150);
-            this.panelUp.TabIndex = 5;
+            this.BtnExportFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExportFile.Font = new System.Drawing.Font("Segoe UI", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnExportFile.Location = new System.Drawing.Point(80, 210);
+            this.BtnExportFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BtnExportFile.Name = "BtnExportFile";
+            this.BtnExportFile.Size = new System.Drawing.Size(120, 45);
+            this.BtnExportFile.TabIndex = 4;
+            this.BtnExportFile.Text = "EXPORT FILE";
+            this.BtnExportFile.UseVisualStyleBackColor = true;
+            this.BtnExportFile.Click += new System.EventHandler(this.button2_Click);
             // 
             // panelMENU
             // 
             this.panelMENU.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panelMENU.Controls.Add(this.iconPictureBox6);
+            this.panelMENU.Controls.Add(this.BtnExit);
+            this.panelMENU.Controls.Add(this.BtnDataView);
+            this.panelMENU.Controls.Add(this.BtnExportFile);
+            this.panelMENU.Controls.Add(this.iconPictureBox4);
+            this.panelMENU.Controls.Add(this.iconPictureBox5);
+            this.panelMENU.Controls.Add(this.panel2);
+            this.panelMENU.Controls.Add(this.BtnAboutUs);
+            this.panelMENU.Controls.Add(this.iconPictureBox1);
+            this.panelMENU.Controls.Add(this.iconPictureBox3);
+            this.panelMENU.Controls.Add(this.BtnLoadFile);
+            this.panelMENU.Controls.Add(this.BtnMapView);
+            this.panelMENU.Controls.Add(this.iconPictureBox2);
             this.panelMENU.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMENU.Location = new System.Drawing.Point(0, 0);
             this.panelMENU.Name = "panelMENU";
-            this.panelMENU.Size = new System.Drawing.Size(267, 750);
+            this.panelMENU.Size = new System.Drawing.Size(220, 808);
             this.panelMENU.TabIndex = 6;
             // 
-            // panel3
+            // panel1
             // 
-            this.panel3.Location = new System.Drawing.Point(476, 316);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(300, 150);
-            this.panel3.TabIndex = 7;
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.iconButton1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(220, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1156, 60);
+            this.panel1.TabIndex = 7;
+            // 
+            // BtnMapView
+            // 
+            this.BtnMapView.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnMapView.Location = new System.Drawing.Point(80, 350);
+            this.BtnMapView.Name = "BtnMapView";
+            this.BtnMapView.Size = new System.Drawing.Size(120, 45);
+            this.BtnMapView.TabIndex = 8;
+            this.BtnMapView.Text = "MAP VIEW";
+            this.BtnMapView.UseVisualStyleBackColor = true;
+            // 
+            // BtnAboutUs
+            // 
+            this.BtnAboutUs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAboutUs.Location = new System.Drawing.Point(80, 420);
+            this.BtnAboutUs.Name = "BtnAboutUs";
+            this.BtnAboutUs.Size = new System.Drawing.Size(120, 45);
+            this.BtnAboutUs.TabIndex = 9;
+            this.BtnAboutUs.Text = "ABOUT US";
+            this.BtnAboutUs.UseVisualStyleBackColor = true;
+            // 
+            // BtnDataView
+            // 
+            this.BtnDataView.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDataView.Location = new System.Drawing.Point(80, 280);
+            this.BtnDataView.Name = "BtnDataView";
+            this.BtnDataView.Size = new System.Drawing.Size(120, 45);
+            this.BtnDataView.TabIndex = 10;
+            this.BtnDataView.Text = "DATA VIEW";
+            this.BtnDataView.UseVisualStyleBackColor = true;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.Location = new System.Drawing.Point(27, 12);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(112, 34);
+            this.iconButton1.TabIndex = 0;
+            this.iconButton1.Text = "iconButton1";
+            this.iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(220, 88);
+            this.panel2.TabIndex = 11;
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.MapMarkerAlt;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 45;
+            this.iconPictureBox1.Location = new System.Drawing.Point(20, 350);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(45, 45);
+            this.iconPictureBox1.TabIndex = 11;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // iconPictureBox2
+            // 
+            this.iconPictureBox2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.iconPictureBox2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.FileAlt;
+            this.iconPictureBox2.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox2.IconSize = 45;
+            this.iconPictureBox2.Location = new System.Drawing.Point(20, 140);
+            this.iconPictureBox2.Name = "iconPictureBox2";
+            this.iconPictureBox2.Size = new System.Drawing.Size(45, 45);
+            this.iconPictureBox2.TabIndex = 12;
+            this.iconPictureBox2.TabStop = false;
+            // 
+            // iconPictureBox3
+            // 
+            this.iconPictureBox3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.iconPictureBox3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox3.IconChar = FontAwesome.Sharp.IconChar.FileImport;
+            this.iconPictureBox3.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox3.IconSize = 45;
+            this.iconPictureBox3.Location = new System.Drawing.Point(20, 210);
+            this.iconPictureBox3.Name = "iconPictureBox3";
+            this.iconPictureBox3.Size = new System.Drawing.Size(45, 45);
+            this.iconPictureBox3.TabIndex = 13;
+            this.iconPictureBox3.TabStop = false;
+            // 
+            // iconPictureBox4
+            // 
+            this.iconPictureBox4.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.iconPictureBox4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox4.IconChar = FontAwesome.Sharp.IconChar.AddressBook;
+            this.iconPictureBox4.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox4.IconSize = 45;
+            this.iconPictureBox4.Location = new System.Drawing.Point(20, 420);
+            this.iconPictureBox4.Name = "iconPictureBox4";
+            this.iconPictureBox4.Size = new System.Drawing.Size(45, 45);
+            this.iconPictureBox4.TabIndex = 14;
+            this.iconPictureBox4.TabStop = false;
+            // 
+            // iconPictureBox5
+            // 
+            this.iconPictureBox5.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.iconPictureBox5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox5.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconPictureBox5.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox5.IconSize = 45;
+            this.iconPictureBox5.Location = new System.Drawing.Point(20, 737);
+            this.iconPictureBox5.Name = "iconPictureBox5";
+            this.iconPictureBox5.Size = new System.Drawing.Size(45, 45);
+            this.iconPictureBox5.TabIndex = 8;
+            this.iconPictureBox5.TabStop = false;
+            // 
+            // iconPictureBox6
+            // 
+            this.iconPictureBox6.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.iconPictureBox6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox6.IconChar = FontAwesome.Sharp.IconChar.Table;
+            this.iconPictureBox6.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox6.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox6.IconSize = 45;
+            this.iconPictureBox6.Location = new System.Drawing.Point(20, 280);
+            this.iconPictureBox6.Name = "iconPictureBox6";
+            this.iconPictureBox6.Size = new System.Drawing.Size(45, 45);
+            this.iconPictureBox6.TabIndex = 9;
+            this.iconPictureBox6.TabStop = false;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.Location = new System.Drawing.Point(80, 737);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(120, 45);
+            this.BtnExit.TabIndex = 10;
+            this.BtnExit.Text = "EXIT";
+            this.BtnExit.UseVisualStyleBackColor = true;
             // 
             // MENU
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1143, 750);
-            this.Controls.Add(this.panel3);
+            this.ClientSize = new System.Drawing.Size(1376, 808);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.panelMENU);
-            this.Controls.Add(this.panelUp);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MENU";
             this.Text = "MENU";
+            this.panelMENU.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox6)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -107,10 +287,21 @@
         #endregion
 
         private OpenFileDialog openFileDialog1;
-        private Button button1;
-        private Button button2;
-        private Panel panelUp;
+        private Button BtnLoadFile;
+        private Button BtnExportFile;
         private Panel panelMENU;
-        private Panel panel3;
+        private Panel panel1;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private Button BtnMapView;
+        private Button BtnAboutUs;
+        private Button BtnDataView;
+        private Panel panel2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox3;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox4;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox6;
+        private Button BtnExit;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox5;
     }
 }
